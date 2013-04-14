b94a8e31653a0c046ec462c6a45821fd55f52633
jesterhazy <jesterhazy@users.noreply.github.com>
2013-04-14 13:20:50 +0000

    assign topics takes optional corpus argument

--- src/main/java/com/bronzespear/hdpa/AssignTopics.java
+++ src/main/java/com/bronzespear/hdpa/AssignTopics.java
@@ -9,16 +9,39 @@ public class AssignTopics {
 	public static void main(String[] args) throws Exception {
 
 		File modelFile = null;
+		File corpusFile = null;
 		
-		if (args.length >= 1) {
-			modelFile = new File(args[0]);
-			
-			if (!modelFile.exists()) {
-				throw new IllegalArgumentException("file does not exist: " + args[0]);
+		for (int i = 0; i < args.length; i++) {
+			switch (i) {
+			case 0:
+				modelFile = new File(args[i]);
+				break;
+			case 1:
+				corpusFile = new File(args[i]);
+				break;
+			default:
 			}
 		}
 		
-		File corpusFile = HdpaUtils.getCorpusForModel(modelFile);
+		if (modelFile == null) {
+			throw new IllegalArgumentException("no model specified!");
+		}
+		
+		
+		if (!modelFile.exists()) {
+			throw new IllegalArgumentException("file does not exist: " + args[0]);
+		}
+		
+		
+		if (corpusFile == null) {
+			corpusFile = HdpaUtils.getCorpusForModel(modelFile);
+		}
+		
+		if (!corpusFile.exists()) {
+			throw new IllegalArgumentException("file does not exist: " + args[1]);
+		}
+		
+		
 		File topicsFile = new File(modelFile.getParentFile(), "doctopics-" + modelFile.getName());
 		
 		CorpusReader corpus = new CorpusReader(corpusFile);
