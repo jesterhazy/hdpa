8c990abc0b604080583dd4340e489a60d72594d7
jesterhazy <jesterhazy@56d363c2-0e28-4097-ba10-be39be3268b3>
2013-04-20 13:26:40 +0000

    fixed infinite loop at eof

--- hdpa/trunk/src/main/java/com/bronzespear/hdpa/solr/PreassignedTopicStrategy.java
+++ hdpa/trunk/src/main/java/com/bronzespear/hdpa/solr/PreassignedTopicStrategy.java
@@ -38,7 +38,7 @@ public class PreassignedTopicStrategy implements TopicStrategy {
 		double[] topics = null;
 
 		int docId = document.getId().intValue();
-		while (currentId < docId) {
+		while (!eof && currentId < docId) {
 			readline();
 		}
 		
@@ -50,42 +50,40 @@ public class PreassignedTopicStrategy implements TopicStrategy {
 	}
 	
 	private void readline() {
-		if (!eof) {
-			String line = null;
+		String line = null;
+		try {
+			line = reader.readLine();
+		} catch (IOException e) {
+			LOG.error(String.format("error reading line %d: %s", linesRead, e.getMessage()), e);
+		}
+		
+		if (line == null) {
+			eof = true;
+			LOG.info("reached end of file at line: " + linesRead);
+		}
+		
+		else {
+			linesRead++;
+			String[] parts = line.split(",");
+			
+			if (currentTopics == null) {
+				currentTopics = new double[parts.length - 1];
+			}
+			
 			try {
-				line = reader.readLine();
-			} catch (IOException e) {
-				LOG.error(String.format("error reading line %d: %s", linesRead, e.getMessage()), e);
-			}
-			
-			if (line == null) {
-				eof = true;
-				LOG.info("reached end of file at line: " + linesRead);
-			}
-			
-			else {
-				linesRead++;
-				String[] parts = line.split(",");
-				
-				if (currentTopics == null) {
-					currentTopics = new double[parts.length - 1];
-				}
-				
-				try {
-					for (int i = 0; i < parts.length; i++) {
-						
-						if (i == 0) {
-							currentId = Integer.parseInt(parts[i]);
-						}
-						
-						else {
-							currentTopics[i - 1] = Double.parseDouble(parts[i]);
-						}
-						
+				for (int i = 0; i < parts.length; i++) {
+					
+					if (i == 0) {
+						currentId = Integer.parseInt(parts[i]);
 					}
-				} catch (NumberFormatException e) {
-					LOG.warn(String.format("invalid format at line %d: [%s]", linesRead, line));
+					
+					else {
+						currentTopics[i - 1] = Double.parseDouble(parts[i]);
+					}
+					
 				}
+			} catch (NumberFormatException e) {
+				LOG.warn(String.format("invalid format at line %d: [%s]", linesRead, line));
 			}
 		}
 	}
