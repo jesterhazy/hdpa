8b959319b21c5585a133ad5e7769546b091f4f9c
jesterhazy <jesterhazy@users.noreply.github.com>
2013-09-13 23:08:13 +0000

    use correct file for eval results

--- src/main/r/plot-hyperparam-settings.R
+++ src/main/r/plot-hyperparam-settings.R
@@ -5,7 +5,7 @@ nytKappaFiles <- matrix(c(
 'nyt-random-t5000-model-b500-k0.6-20130528-0559', 'coherence-20130529-2152.txt', 'eval-20130530-0030.txt',
 'nyt-random-t5000-model-b500-k0.7-20130528-0611', 'coherence-20130530-0723.txt', 'eval-20130530-1001.txt',
 'nyt-random-t5000-model-b500-k0.8-20130528-0614', 'coherence-20130530-0723.txt', 'eval-20130530-1025.txt',
-'nyt-random-t5000-model-b500-k0.9-20130529-0546', 'coherence-20130531-0455.txt', 'eval-20130531-0756.txt',
+'nyt-random-t5000-model-b500-k0.9-20130529-0546', 'coherence-20130531-0455.txt', 'eval-20130531-0600.txt',
 'nyt-random-t5000-model-b500-k1.0-20130528-0621', 'coherence-20130531-1702.txt', 'eval-20130531-2002.txt'
 ), 6, 3, byrow=T)
 
@@ -14,7 +14,7 @@ nytBatchFiles <- matrix(c(
 'nyt-random-t5000-model-b10-k0.9-20130528-0627', 'coherence-20130605-0831.txt', 'eval-20130601-1646.txt',
 'nyt-random-t5000-model-b50-k0.9-20130528-0640', 'coherence-20130530-1948.txt', 'eval-20130531-1256.txt',
 'nyt-random-t5000-model-b100-k0.9-20130528-0648', 'coherence-20130531-1706.txt', 'eval-20130601-0218.txt',
-'nyt-random-t5000-model-b500-k0.9-20130529-0546', 'coherence-20130531-0455.txt', 'eval-20130531-0756.txt',
+'nyt-random-t5000-model-b500-k0.9-20130529-0546', 'coherence-20130531-0455.txt', 'eval-20130531-0600.txt',
 'nyt-random-t5000-model-b1000-k0.9-20130528-0653', 'coherence-20130530-2115.txt', 'eval-20130530-2324.txt'
 ), 5, 3, byrow=T)
 
@@ -23,7 +23,7 @@ nanKappaFiles <- matrix(c(
 'nan-random-t5000-model-b500-k0.6-20130528-0656', 'coherence-20130529-0709.txt', 'eval-20130529-0901.txt',
 'nan-random-t5000-model-b500-k0.7-20130528-0656', 'coherence-20130529-0755.txt', 'eval-20130529-0959.txt',
 'nan-random-t5000-model-b500-k0.8-20130528-0703', 'coherence-20130529-0803.txt', 'eval-20130529-1013.txt',
-'nan-random-t5000-model-b500-k0.9-20130529-0546', 'coherence-20130530-1952.txt', 'eval-20130530-2209.txt',
+'nan-random-t5000-model-b500-k0.9-20130529-0546', 'coherence-20130530-1952.txt', 'eval-20130530-2108.txt',
 'nan-random-t5000-model-b500-k1.0-20130528-0703', 'coherence-20130529-1559.txt', 'eval-20130529-1807.txt'
 ), 6, 3, byrow=T)
 
@@ -31,7 +31,7 @@ nanBatchFiles <- matrix(c(
 'nan-random-t5000-model-b10-k0.9-20130528-0703', 'coherence-20130531-0456.txt', 'eval-20130601-1256.txt',
 'nan-random-t5000-model-b50-k0.9-20130528-0907', 'coherence-20130529-2150.txt', 'eval-20130530-0551.txt',
 'nan-random-t5000-model-b100-k0.9-20130528-0907', 'coherence-20130529-2149.txt', 'eval-20130530-0258.txt',
-'nan-random-t5000-model-b500-k0.9-20130529-0546', 'coherence-20130530-1952.txt', 'eval-20130530-2209.txt',
+'nan-random-t5000-model-b500-k0.9-20130529-0546', 'coherence-20130530-1952.txt', 'eval-20130530-2108.txt',
 'nan-random-t5000-model-b1000-k0.9-20130528-0808', 'coherence-20130529-1044.txt', 'eval-20130529-1229.txt'
 ), 5, 3, byrow=T)
 
